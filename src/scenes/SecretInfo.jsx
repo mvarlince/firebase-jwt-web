7a4ad8b9450d2f2609006ceac5029391d434f784
Varlince Meran <varlincemeran@gmail.com>
2023-02-27 14:32:08 -0500

    connected to API

--- src/scenes/SecretInfo.jsx
+++ src/scenes/SecretInfo.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react"
 
 
 export default function SecretInfo(){
+    const [secretStuff, setSecretStuff] = useState()
+    // make a secure call to our API to get the secret info
+    useEffect( () => {
+        fetch('http://localhost:4040/secrets')
+            .then(res => res.json())
+            .then(data => setSecretStuff(data.message))
+            .catch(alert)
+    },[] )
     return (
         <>
         <h1>
             Secret Info
         </h1>
+            {secretStuff
+             ? <h2> {secretStuff} </h2>
+                :<h2> Loading </h2>
+            } 
         </>
     )
 }
