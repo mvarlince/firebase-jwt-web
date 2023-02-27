3c200e88bc14e4a8d7ae7d37a16ef5f90a7943e2
Varlince Meran <varlincemeran@gmail.com>
2023-02-27 15:09:31 -0500

    setting token

--- src/scenes/SecretInfo.jsx
+++ src/scenes/SecretInfo.jsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react"
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeOvnGAgXfb7eIf-UymHDb_P65qr9FuSA",
+    authDomain: "fb-jwt-vm.firebaseapp.com",
+    projectId: "fb-jwt-vm",
+    storageBucket: "fb-jwt-vm.appspot.com",
+    messagingSenderId: "658977927917",
+    appId: "1:658977927917:web:985834e5833c19daf452e4"
+  };
 
 
 export default function SecretInfo(){
     const [secretStuff, setSecretStuff] = useState()
     // make a secure call to our API to get the secret info
     useEffect( () => {
-        fetch('http://localhost:4040/secrets')
-            .then(res => res.json())
-            .then(data => setSecretStuff(data.message))
+        const app = initializeApp(firebaseConfig)
+        const auth = getAuth(app)
+        auth.currentUser.getIdToken(false) // true get a new token no matter what --- false, if user has token past 30 mins, use it
+            .then( token => {
+                fetch('http://localhost:4040/secrets',{ //SEND TOKEN
+                        headers: {Authorization: token}
+                    })
+                    .then(res => res.json())
+                    .then(data => setSecretStuff(data.message))
+            })
             .catch(alert)
     },[] )
     return (
